d642bdbdb511f32a08cc46f0a496618a985d6c2e
Florian Jüngermann <florianjuengermann@gmail.com>
2020-06-05 00:07:35 +0200

    added spooler performance logging if full

--- Spooler.py
+++ Spooler.py
@@ -8,12 +8,15 @@ class Spooler:
 		self._q = queue.Queue()
 		self._function = function
 		self._timeInterval = timeInterval
+		self._threadCount = threadCount
 		self._name = name
 		for i in range(threadCount):
 			Thread(target=self._run, name=name + " spooler #" + str(i)).start()
 
 	def put(self, *posArgs, callback=None, **kwArgs):
 		info = {'callback':callback, 'posArgs':posArgs, 'kwArgs':kwArgs}
+		if self._q.qsize() >= self._threadCount:
+			logger.debug(f"Spooler full! Queue size {self._q.qsize()}/{self._threadCount}.")
 		self._q.put(info)
 
 	def _run(self):
