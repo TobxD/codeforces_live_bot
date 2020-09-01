a722540283d0870d01815afa031f6e6a76b7d1e9
Florian Jüngermann <florianjuengermann@gmail.com>
2020-09-02 00:39:06 +0200

    🗑 auto deleting old settings messages

--- commands/settings.py
+++ commands/settings.py
@@ -1,18 +1,34 @@
+from __future__ import annotations
 import json
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+	from telegram.Chat import Chat
+
 from utils import database as db
 from telegram import telegram as tg
 from codeforces import codeforces as cf
 from utils import util
 from utils.util import logger
 from commands import bot
-from telegram import Chat
+from telegram import Chat as ChatFunctions
 from commands import general_settings, notification_settings as notify_settings, behavior_settings, widthSelector
 
-def handleSettings(chat, req):
+
+
+def handleSettings(chat:Chat, req):
+	def deleteOldSettings(chat:Chat):
+		if chat.settings_msgid:
+			chat.deleteMessage(chat.settings_msgid)
+			chat.settings_msgid = None
+	def callbackSetMsgId(id):
+		if id != False:
+			chat.settings_msgid = id
+
 	bot.setOpenCommandFunc(chat.chatId, None)
+	deleteOldSettings(chat)
 	buttons = getSettingsButtons()
 	replyMarkup = getReplyMarkup(buttons)
-	chat.sendMessage("What do you want to change?", replyMarkup)
+	chat.sendMessage("What do you want to change?", replyMarkup, callbackSetMsgId)
 
 def getReplyMarkup(inlineKeyboard):
 	replyMarkup = {"inline_keyboard": inlineKeyboard}
@@ -28,7 +44,7 @@ def getSettingsButtons():
 	return buttons
 
 def handleCallbackQuery(callback):
-	chat = Chat.getChat(str(callback['message']['chat']['id']))
+	chat = ChatFunctions.getChat(str(callback['message']['chat']['id']))
 	data = callback['data']
 
 	if not ":" in data:
@@ -49,7 +65,7 @@ def handleCallbackQuery(callback):
 		retMsg = funs[pref](chat, suff, callback)
 		tg.sendAnswerCallback(chat.chatId, callback['id'], retMsg)
 
-def handleSettingsCallback(chat, data, callback):
+def handleSettingsCallback(chat:Chat, data, callback):
 	if data != "":
 		logger.critical("Invalid callback settings data: " + data)
 	else:
