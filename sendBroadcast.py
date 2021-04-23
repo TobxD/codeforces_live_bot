1fa78fc21ff69b0e3bad3bbc909a829d0e248ff2
Florian Jüngermann <florianjuengermann@gmail.com>
2021-04-23 18:38:28 +0200

    added time conversion support for sendBroadcast

--- sendBroadcast.py
+++ sendBroadcast.py
@@ -1,4 +1,4 @@
-import sys, os, time
+import sys, os, time, re, datetime
 from telegram import Chat
 from utils import util
 from utils.util import logger
@@ -12,10 +12,22 @@ if len(sys.argv) != 1:
 		msgText = open(sys.argv[1]).read()[:-1] # discard trailing newline
 		logger.info("sending broadcast message:\n" + msgText)
 		for chatId in Chat.chats:
-			Chat.chats[chatId].sendMessage(msgText)
+			msg = msgText
+			chat = Chat.chats[chatId]
+			while (m := re.search("\[%t [0-9]*\]", msg)):
+				tInSec = int(m.group()[4: -1])
+				timeLeft = int(tInSec - time.time())
+				delta = datetime.timedelta(seconds=timeLeft)
+				dTime = f"*{util.displayTime(tInSec, chat.timezone)}* (in {':'.join(str(delta).split(':')[:2])} hours)"
+				msg = msg[:m.span()[0]] + + msg[m.span()[1]:]
+			print(f"chat: {chatId}: {msg}")
+			#chat.sendMessage(msg)
 		time.sleep(1)
 		while tg.requestSpooler._q[0].qsize() > 0:
-			print("waiting")
+			try:
+				print(f"waiting {tg.requestSpooler._q[0].qsize()}")
+			except Exception as ex:
+				print(ex)
 			time.sleep(1)
 		logger.info("sending broadcasts finished")
 	except Exception as e:
